69c7b9a498818d53b2e5e8d2ae14dd9041ed8e21
Avi-GG <avigtt15@gmail.com>
2024-10-10 19:22:36 +0530

    updated

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const Signup = () => {
 	const [username, setUsername] = useState("");
